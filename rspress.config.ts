d469ac05ee3972f623774f93258d1fcb134ad193
lorien <1173381395@qq.com>
2025-04-03 20:56:32 +0800

    修改统计代码

--- rspress.config.ts
+++ rspress.config.ts
@@ -7,6 +7,21 @@ import readingTime from 'rspress-plugin-reading-time'
 
 export default defineConfig({
   root: 'docs',
+  head: [
+    [
+      'script',
+      {},
+      `
+      var _hmt = _hmt || [];
+      (function() {
+          var hm = document.createElement("script");
+          hm.src = "https://hm.baidu.com/hm.js?59d12908ad3d91c301a4851e08586997";
+          var s = document.getElementsByTagName("script")[0]; 
+          s.parentNode.insertBefore(hm, s);
+      })();
+      `
+    ]
+  ],
   base: '/',
   title: 'Lorien Blog',
   icon: '/logo.png',
